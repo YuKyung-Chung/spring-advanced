5036146f65962534108291f4b5e9dd6844636ae8
YuKyung-Chung <kadie@naver.com>
2023-01-25 00:17:44 +0900

    스프링 AOP 구현5 - 어드바이스 순서

--- aop/src/main/java/hello/aop/order/aop/AspectV4Pointcut.java
+++ aop/src/main/java/hello/aop/order/aop/AspectV4Pointcut.java
@@ -4,11 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
-public class AspectV4 {
+public class AspectV4Pointcut {
 
     @Around("hello.aop.order.aop.Pointcuts.allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
